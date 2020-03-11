a5da9d599174f87f02c833b80790f226c41ff9b8
joannasroka <joannasroka98@gmail.com>
2020-03-11 21:39:48 +0100

    add fourth list

---
+++ lab4_zajecia/lab4_zajecia/FileLastError.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "FileLastError.h"
+
+
+FileLastError::FileLastError()
+{
+}
+
+
+FileLastError::~FileLastError()
+{
+}
+
+bool FileLastError::getLastError(bool lastError)
+{
+	return lastError;
+}
